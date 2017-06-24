f18053bf0c4ffdedbc3627b195ec41c3e5a4d38e
hsjung22 <hsjung22@gmail.com>
2017-06-24 16:24:16 -0700

    requirement 2

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,7 +1,37 @@
 $( document ).ready(function() {
-  let catCount = 0;
-  $(".cat-click").on( "click", function() {
-    catCount++;
-    $(".cat-click-counter").html(catCount);
+
+  let catCollection = [
+    {
+      name: 'poplinre',
+      count: 0
+    },
+    {
+      name: 'chewie',
+      count: 0
+    }
+  ];
+
+  function catCounterDisplay(name, count){
+    let className = name + "-count-display"
+    $("." + className).html("Click Counter: " + count);
+  }
+
+  $.map( catCollection, function( cat, i ) {
+    // image element with click function that'll add 1 to the count
+    $( "<img/>", {
+      src: "src/images/cat-" + cat.name + ".jpg",
+      height: "150px",
+      click: function() {
+        cat.count++;
+        catCounterDisplay(cat.name, cat.count);
+      }
+    }).appendTo( "body" );
+    //p tag element to display the updated count
+    $("<p/>", {
+      class: cat.name +"-count-display",
+      text: "Click Counter: 0"
+    }).appendTo("body");
   });
+
+
 });
