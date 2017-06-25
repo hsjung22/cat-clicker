115d5079f03b0d19b8e4bc18852194e8d4497a1f
hsjung22 <hsjung22@gmail.com>
2017-06-25 12:59:00 -0700

    cat clicker premium requirements

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -8,29 +8,55 @@ $( document ).ready(function() {
     {
       name: 'chewie',
       count: 0
+    },
+    {
+      name: 'jetske',
+      count: 0
+    },
+    {
+      name: 'tom',
+      count: 0
+    },
+    {
+      name: 'garfield',
+      count: 0
     }
   ];
 
-  function catCounterDisplay(name, count){
-    let className = name + "-count-display"
-    $("." + className).html("Click Counter: " + count);
+  function catCounterDisplay(cat){
+    let className = cat.name + "-count-display"
+    $("." + className).html("Click Counter: " + cat.count);
   }
 
-  $.map( catCollection, function( cat, i ) {
-    // image element with click function that'll add 1 to the count
-    $( "<img/>", {
-      src: "src/images/cat-" + cat.name + ".jpg",
-      height: "150px",
-      click: function() {
-        cat.count++;
-        catCounterDisplay(cat.name, cat.count);
-      }
-    }).appendTo( "body" );
-    //p tag element to display the updated count
+  function selectedCatDisplay(cat){
+    // display cat image
+    $('.cat-display').html(
+      $( "<img/>", {
+        src: "src/images/" + cat.name + ".jpg",
+        height: "150px",
+        click: function() {
+          cat.count++;
+          catCounterDisplay(cat);
+        }
+      })
+    )
+    // display counter
     $("<p/>", {
       class: cat.name +"-count-display",
-      text: "Click Counter: 0"
-    }).appendTo("body");
+      text: "Click Counter: " + cat.count
+    }).appendTo($('.cat-display'));
+
+  }
+
+  // display the names of all the cats on the left of the screen
+  // clicking on each name will display its picture with its count
+  $.map( catCollection, function( cat, i ) {
+    $("<h4/>", {
+      text: cat.name,
+      click: function() {
+        selectedCatDisplay(cat);
+      }
+    }).appendTo( $('.cat-name-list') );
   });
 
 
