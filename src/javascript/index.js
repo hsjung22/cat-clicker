8a254ad1aac7ed88ed9598d1529019269f2b228e
hsjung22 <hsjung22@gmail.com>
2017-07-20 21:04:30 -0700

    feat: Add admin functionality

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,32 +1,46 @@
 $( document ).ready(function() {
 
   var model = {
-    catCollection:[
-                    {
-                      name: 'poplinre',
-                      count: 0
-                    },
-                    {
-                      name: 'chewie',
-                      count: 0
-                    },
-                    {
-                      name: 'jetske',
-                      count: 0
-                    },
-                    {
-                      name: 'tom',
-                      count: 0
-                    },
-                    {
-                      name: 'garfield',
-                      count: 0
-                    },
-                    {
-                      name: 'dave',
-                      count: 0
-                    }
-                  ],
+    adminStatus: false,
+    catCollection:
+      [
+        {
+          name: 'poplinre',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/poplinre.jpg"
+        },
+        {
+          name: 'chewie',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/chewie.jpg"
+        },
+        {
+          name: 'jetske',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/jetske.jpg"
+        },
+        {
+          name: 'tom',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/tom.jpg"
+        },
+        {
+          name: 'garfield',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/garfield.jpg"
+        },
+        {
+          name: 'dave',
+          count: 0,
+          imgAttribution: "google.com/images",
+          imgSource: "src/images/dave.jpg"
+        }
+      ],
 
     getAllCats: function() {
       return this.catCollection;
@@ -34,9 +48,19 @@ $( document ).ready(function() {
 
     addCount: function(cat) {
       cat.count++;
+    },
+
+    toggleAdminStatus: function() {
+      this.adminStatus = !this.adminStatus;
+    },
+
+    getAdminStatus: function() {
+      return this.adminStatus;
+    },
+
+    updateCat: function(cat, updateField) {
+      $.extend(cat, updateField);
     }
-
-
   };
 
   var octopus = {
@@ -51,17 +75,54 @@ $( document ).ready(function() {
       catSelected: function(cat) {
         view.renderCat(cat);
         view.renderCount(cat);
+        this.renderAdminSection(cat);
+      },
+
+      toggleAdmin: function(cat) {
+        model.toggleAdminStatus();
+        this.renderAdminSection(cat);
       },
 
       addCount: function(cat) {
         model.addCount(cat);
         view.renderCount(cat);
+        this.rerenderCat(cat);
+      },
+
+      updateCat: function(cat, updateField) {
+        model.updateCat(cat, updateField);
+        model.toggleAdminStatus();
+        this.rerenderCat(cat);
+      },
+
+      rerenderCat: function(cat) {
+        view.renderCatNameList();
+        view.renderCount(cat);
+        this.renderAdminSection(cat);
+      },
+
+      renderAdminSection: function(cat) {
+        if (model.getAdminStatus()){
+          view.renderAdminForm(cat, model.getAdminStatus());
+        } else {
+          view.renderAdminButton(cat);
+        }
       }
   };
 
   var view = {
       renderCatNameList: function() {
-        $.map( octopus.getAllCats(), function( cat, i ) {
+        $('.cat-list-container').html(
+          $( "<div/>", {
+            class: "cat-name-list"
+          })
+        )
+
+        this.buildCatNameList()
+      },
+
+      buildCatNameList: function() {
+        $.map( octopus.getAllCats(), function(cat, i) {
           $("<h4/>", {
             text: cat.name,
             click: function() {
@@ -75,7 +136,7 @@ $( document ).ready(function() {
         // display cat image
         $('.cat-picture').html(
           $( "<img/>", {
-            src: "src/images/" + cat.name + ".jpg",
+            src: cat.imgSource,
             height: "150px",
             click: function() {
               octopus.addCount(cat);
@@ -92,7 +153,81 @@ $( document ).ready(function() {
             text: "Click Counter: " + cat.count
           })
         )
+      },
+
+      renderAdminButton: function(cat) {
+        $(".admin").html(
+          $("<button>", {
+            text: 'Admin',
+            click: function() {
+              octopus.toggleAdmin(cat);
+            }
+          })
+        )
+      },
+
+      renderAdminForm: function(cat) {
+        $(".admin").html(
+          $("<form>", {
+            class: 'admin-form'
+          })
+        )
+
+        this.appendNameInput(cat)
+        this.appendClickInput(cat)
+        this.appendURLInput(cat)
+        this.appendFormSubmit(cat)
+        this.cancelAdmin(cat)
+      },
+
+      appendNameInput: function(cat) {
+        $("<input>", {
+          class: 'cat-name-input',
+          placeholder: 'Name',
+          value: cat.name
+        }).appendTo( $('.admin-form') );
+      },
+
+      appendClickInput: function(cat) {
+        $("<input>", {
+          class: 'cat-click-input',
+          placeholder: 'Click #',
+          value: cat.count
+        }).appendTo( $('.admin-form') );
+      },
+
+      appendURLInput: function(cat) {
+        $("<input>", {
+          class: 'cat-url-input',
+          placeholder: 'URL',
+          value: cat.imgAttribution
+        }).appendTo( $('.admin-form') );
+      },
+
+      appendFormSubmit: function(cat) {
+        $("<input>", {
+          value: "Submit",
+          type: "button",
+          click: function() {
+            octopus.updateCat(cat, {
+              imgAttribution: $('.cat-url-input').val(),
+              count: $('.cat-click-input').val(),
+              name: $('.cat-name-input').val()
+            })
+          }
+        }).appendTo( $('.admin-form') );
+      },
+
+      cancelAdmin: function(cat) {
+        $("<input>", {
+          value: "Cancel",
+          type: "button",
+          click: function() {
+            octopus.toggleAdmin(cat);
+          }
+        }).appendTo( $('.admin-form') );
       }
+
   };
 
   octopus.init();
